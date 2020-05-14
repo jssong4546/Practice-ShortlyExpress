859582813be30fe2730a58f2e8457827d127520b
unknown <songal4546@gmail.com>
2020-05-14 15:25:54 +0900

    ec2

--- server/config/config.js
+++ server/config/config.js
@@ -8,5 +8,25 @@ module.exports = {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
+    test: {
+      username: 'songal4546',
+      password: process.env.DATABASE_PASSWORD,
+      database: 'shortly',
+      host:
+        'practice-database-deploy.cfs24azdcpaq.ap-northeast-2.rds.amazonaws.com',
+      port: 3306,
+      dialect: 'mysql',
+      logging: false,
+    },
+    production: {
+      username: 'songal4546',
+      password: process.env.DATABASE_PASSWORD,
+      database: 'shortly',
+      host:
+        'practice-database-deploy.cfs24azdcpaq.ap-northeast-2.rds.amazonaws.com',
+      port: 3306,
+      dialect: 'mysql',
+      logging: false,
+    },
   },
 };
