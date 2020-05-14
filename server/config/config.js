15638432ffde21e1939ec57c24a52174f875d560
unknown <songal4546@gmail.com>
2020-05-14 23:31:35 +0900

    env

--- server/config/config.js
+++ server/config/config.js
@@ -3,16 +3,17 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'root',
-    password: 'qweasd1!5057',
+    password: process.env.DATABASE_PASSWORD,
     database: 'shortly',
     host: 'localhost',
+    port: 3306,
     dialect: 'mysql',
     logging: false,
     ssl: true,
   },
   test: {
     username: 'songal4546',
-    password: 'qweasd1!5057',
+    password: process.env.DATABASE_PASSWORD,
     database: 'shortly',
     host:
       'practice-database-deploy.cfs24azdcpaq.ap-northeast-2.rds.amazonaws.com',
@@ -23,7 +24,7 @@ module.exports = {
   },
   production: {
     username: 'songal4546',
-    password: 'qweasd1!5057',
+    password: process.env.DATABASE_PASSWORD,
     database: 'shortly',
     host:
       'practice-database-deploy.cfs24azdcpaq.ap-northeast-2.rds.amazonaws.com',
